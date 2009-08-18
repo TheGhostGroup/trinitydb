7dab2b64760c3a259b9c277ef93e6223b69ac76b
runningnak3d <runningnak3d@localhost>
2009-08-18 16:33:40 +0000

    * Cleaning up for a fresh start! * Coming soon...

--- updates/5_corepatch_characters_51_to_250.sql
+++
@@ -1,6 +0,0 @@
--- 79
-ALTER TABLE characters
-  CHANGE COLUMN gmstate extra_flags int(11) unsigned NOT NULL default '0';
--- 220
-ALTER TABLE `characters`
-    ADD `latency` int(11) unsigned NOT NULL default '0' AFTER `taxi_path`;
